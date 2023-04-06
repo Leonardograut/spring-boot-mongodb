25dfa8aeffcf99542e3887954add3188de4f7110
Leonardo Nascimento <omgleo@live.com>
2023-04-06 16:27:55 -0300

    Obtendo um post por id

---
+++ src/main/java/com/projetomongo/demo/Service/PostService.java
@@ -0,0 +1,25 @@
+package com.projetomongo.demo.Service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetomongo.demo.Service.exception.ObjectNotFoundException;
+import com.projetomongo.demo.UsuarioRepositorio.PostRepository;
+import com.projetomongo.demo.entidade.Post;
+
+@Service
+public class PostService {
+    
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        Optional<Post> obj = postRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        
+    
+    }
+
+}
