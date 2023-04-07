80d9c8829d2d448d18f61c5d801ed3d93654806f
Leonardo Nascimento <omgleo@live.com>
2023-04-06 22:42:49 -0300

    Consulta simples com query methods

--- src/main/java/com/projetomongo/demo/controller/PostController.java
+++ src/main/java/com/projetomongo/demo/controller/PostController.java
@@ -1,13 +1,17 @@
 package com.projetomongo.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetomongo.demo.Service.PostService;
+import com.projetomongo.demo.controller.util.URL;
 import com.projetomongo.demo.entidade.Post;
 
 @RestController
@@ -24,5 +28,13 @@ public class PostController {
     public ResponseEntity<Post>findById(@PathVariable String id){ 
            Post obj = service.findById(id);    
         return ResponseEntity.ok().body(obj);
+    }
+
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "")String text){ 
+           text = URL.decodeParam(text);
+           List<Post> list =service.findByTitle(text);
+     return ResponseEntity.ok().body(list);
     }
 }
